9ee670d65ea7df9dac09489d7b6cef9c38dfcb33
scoo73r <me@scooter.engineering>
2024-03-29 20:52:16 -0500

    Update lexer with let

--- token/token.go
+++ token/token.go
@@ -48,3 +48,15 @@ type Token struct {
 
 	Literal string
 }
+
+var keywords = map[string]TokenType{
+	"fn": FUNCTION,
+	"let": LET,
+}
+
+func LookupIdentifier (identifier string) TokenType {
+	if tok, ok := keywords[identifier]; ok {
+		return tok
+	}
+	return IDENTIFIER
+}
