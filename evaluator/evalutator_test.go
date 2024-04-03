e555025dba08745ff0a2d43daae084c2e9cbc222
scoo73r <me@scooter.engineering>
2024-04-02 22:57:44 -0500

    add identifiers

--- evaluator/evalutator_test.go
+++ evaluator/evalutator_test.go
@@ -194,6 +194,10 @@ if (10 > 1) {
 `,
 			"unknown operator: BOOLEAN + BOOLEAN",
 		},
+		{
+			"foobar",
+			"identifier not found: foobar",
+		},
 	}
 
     for _, tt := range tests {
@@ -211,6 +215,22 @@ if (10 > 1) {
 	}
 }
 
+func TestLetStatement(t *testing.T) {
+	tests := []struct {
+		input string
+		expected int64
+	} {
+		{"let a = 5; a;", 5},
+		{"let a = 5 * 5; a;", 25},
+		{"let a = 5; let b = a; b;", 5},
+		{"let a = 5; let b = a; let c = a + b + 5; c;", 15},
+	}
+
+	for _, tt := range tests {
+		testIntegerObject(t, testEval(tt.input), tt.expected)
+	}
+}
+
 func testNullObject(t *testing.T, obj object.Object) bool {
 	if obj != NULL {
 		t.Errorf("object is not NULL. got=%T (%v)", obj, obj)
@@ -223,7 +243,8 @@ func testEval(input string) object.Object {
 	l := lexer.New(input)
 	p:= parser.New(l)
 	program := p.ParseProgram()
-	return Eval(program)
+	env := object.NewEnvironment()
+	return Eval(program, env)
 }
 
 func testIntegerObject(t *testing.T, obj object.Object, expected int64) bool {
