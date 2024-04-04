5365a7721346ea0fbe4db393ccab2511338ad849
scoo73r <me@scooter.engineering>
2024-04-03 20:49:58 -0500

    add function to objects

--- object/object.go
+++ object/object.go
@@ -1,8 +1,10 @@
 package object
 
 import (
+	"bytes"
 	"fmt"
 	"scooter/monkey/ast"
+	"strings"
 )
 
 const (
@@ -73,4 +75,21 @@ type Function struct {
 	Paramaters []*ast.Identifier
 	Body *ast.BlockStatement
 	Env *Environment
+}
+
+func (f *Function) Type() ObjectType { return FUNCTION_OBJ}
+func (f *Function) Inspect() string { 
+	var out bytes.Buffer
+
+	params := []string{}
+	for _, p := range f.Paramaters{
+		params = append(params, p.String())
+	}
+	out.WriteString("fn")
+	out.WriteString("(")
+	out.WriteString(strings.Join(params, ", "))	
+	out.WriteString(") {\n")
+	out.WriteString(f.Body.String())
+	out.WriteString("\n")
+	return out.String()
 }
